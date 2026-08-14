c8cffdca5497d93ea5ea47c1244a06e172cc8579
Moviemania12 <movie3.mania@gmail.com>
2026-08-14 11:47:11 +0530

    fix(routing): redirect dc-map to data-center-map

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/dc-map",
+        destination: "/data-center-map",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
